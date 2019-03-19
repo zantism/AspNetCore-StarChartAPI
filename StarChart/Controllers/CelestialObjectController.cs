73cc695c50e5718ef251cb2aff51f28849659bf4
zmoye <zantis.moye@live.com>
2019-03-19 16:48:21 -0500

    Update CelestialObjectController.cs

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -8,7 +8,15 @@ using StarChart.Data;
 
 namespace StarChart.Controllers
 {
+
+    [Route("")]
+    [ApiController]
     public class CelestialObjectController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context) {
+            _context = context;
+        }
     }
 }
