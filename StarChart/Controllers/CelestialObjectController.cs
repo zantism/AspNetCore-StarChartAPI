f568b4d7043376413ce7dd938b6f64917d87a54b
zmoye <zantis.moye@live.com>
2019-03-19 16:46:50 -0500

    Create CelestialObjectController.cs

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController : ControllerBase
+    {
+    }
+}
